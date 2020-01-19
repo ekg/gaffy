ea94946015e7a3a0f6a93a17215a199a2c8ad9f3
Erik Garrison <erik.garrison@gmail.com>
2020-01-19 22:01:48 +0100

    make output kind of useful

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
-extern crate clap;
-
-use clap::{Arg, App};
+//use std::slice;
+//use std::str;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+
+extern crate clap;
+use clap::{Arg, App};
+
+fn do_vectorize(reader: BufReader<File>, trim_read_name: bool) {
+    println!("name\tquery.length\tnode.id\trank");
+    for line in reader.lines() {
+        // parse the line
+        let l = line.unwrap();
+        //println!("{}", l);
+        let mut i = 0;
+        let mut name = "";
+        let mut path = "";
+        let mut query_length: u64 = 0;
+        for s in l.split("\t") {
+            i = i + 1;
+            match i {
+                1 => name = if trim_read_name { s.split_ascii_whitespace().nth(0).unwrap() } else { s },
+                2 => query_length = s.parse::<u64>().unwrap(),
+                6 => path = s,
+                _ => { },
+            };
+        }
+        i = 0;
+        for n in path.split(|c| c == '<' || c == '>') {
+            if !n.is_empty() {
+                i += 1;
+                println!("{}\t{}\t{}\t{}", name, query_length, n, i);
+            }
+        }
+    }
+}
 
 fn main() -> io::Result<()> {
     let matches = App::new("gaffy")
@@ -18,14 +49,17 @@ fn main() -> io::Result<()> {
              .short("v")
              .long("vectorize")
              .help("Write a tabular representation of the alignments (one record per alignment node traversal)"))
+        .arg(Arg::with_name("trim-read-name")
+             .short("t")
+             .long("trim-read-name")
+             .help("Trim the read name at the first whitespace"))
         .get_matches();
     let filename = matches.value_of("INPUT").unwrap();
-    println!("{}", filename);
+    //println!("{}", filename);
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
-    for line in reader.lines() {
-        // parse the line
-        println!("{}", line?);
+    if matches.is_present("vectorize") {
+        do_vectorize(reader, matches.is_present("trim-read-name"));
     }
     Ok(())
 }

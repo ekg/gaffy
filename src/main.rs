ceb375baaa318aae768aa3308f312e69b8c2088d
Erik Garrison <erik.garrison@gmail.com>
2020-01-20 12:08:52 +0100

    update with better matrix output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-//use std::slice;
-//use std::str;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 extern crate clap;
 use clap::{Arg, App};
 
-fn do_vectorize(reader: BufReader<File>, trim_read_name: bool) {
+fn do_vectorize(filename: &str, trim_read_name: bool) {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
     println!("name\tquery.length\tnode.id\trank");
     for line in reader.lines() {
         // parse the line
@@ -35,6 +35,72 @@ fn do_vectorize(reader: BufReader<File>, trim_read_name: bool) {
     }
 }
 
+fn gaf_max_min_id(filename: &str) -> (usize, usize) {
+    let mut max_id = usize::min_value();
+    let mut min_id = usize::max_value();
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let l = line.unwrap();
+        let path = l.split("\t").nth(6).unwrap();
+        for n in path.split(|c| c == '<' || c == '>') {
+            if !n.is_empty() {
+                let id = n.parse::<usize>().unwrap();
+                if id > max_id {
+                    max_id = id;
+                }
+                if id < min_id {
+                    min_id = id;
+                }
+            }
+        }
+    }
+    (min_id, max_id)
+}
+
+fn do_matrix(filename: &str, trim_read_name: bool) {
+    let (_min_id, max_id) = gaf_max_min_id(filename);
+    print!("aln.name\tquery.length\tnode.count");
+    for x in 1..=max_id {
+        print!("\tnode.{}", x);
+    }
+    print!("\n");
+    io::stdout().flush().unwrap();
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        // parse the line
+        let l = line.unwrap();
+        let mut name = "";
+        let mut path = "";
+        let mut query_length: u64 = 0;
+        for (i,s) in l.split("\t").enumerate() {
+            match i {
+                0 => name = if trim_read_name { s.split_ascii_whitespace().nth(0).unwrap() } else { s },
+                1 => query_length = s.parse::<u64>().unwrap(),
+                5 => path = s,
+                _ => { },
+            };
+        }
+        let mut v = vec![0; max_id];
+        for n in path.split(|c| c == '<' || c == '>') {
+            if !n.is_empty() {
+                let id = n.parse::<usize>().unwrap();
+                v[id-1] = 1;
+            }
+        }
+        print!("{}", name);
+        print!("\t{}", query_length);
+        let sum: u64 = v.iter().sum();
+        print!("\t{}", sum);
+        for x in v {
+            print!("\t{}", x);
+        }
+        print!("\n");
+        io::stdout().flush().unwrap(); // maybe not necessary
+    }
+}
+
 fn main() -> io::Result<()> {
     let matches = App::new("gaffy")
         .version("0.1.0")
@@ -53,13 +119,17 @@ fn main() -> io::Result<()> {
              .short("t")
              .long("trim-read-name")
              .help("Trim the read name at the first whitespace"))
+        .arg(Arg::with_name("matrix")
+             .short("m")
+             .long("matrix")
+             .help("Write a binary matrix representing coverage across the graph (two passes over input file)."))
         .get_matches();
     let filename = matches.value_of("INPUT").unwrap();
     //println!("{}", filename);
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
     if matches.is_present("vectorize") {
-        do_vectorize(reader, matches.is_present("trim-read-name"));
+        do_vectorize(filename, matches.is_present("trim-read-name"));
+    } else if matches.is_present("matrix") {
+        do_matrix(filename, matches.is_present("trim-read-name"));
     }
     Ok(())
 }
